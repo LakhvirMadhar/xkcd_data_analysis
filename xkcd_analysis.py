9ef23bf5c53fc9068562942181c36582b2c1a625
Lakhvir Madhar <lakhvirmadhar36@gmail.com>
2024-09-18 11:21:08 -0400

    Update 02 to XKCD Analysis
    - Fixed a bug where the latest comic in the json would get added twice
    - Answered and plotted some analysis questions

--- xkcd_analysis.py
+++ xkcd_analysis.py
@@ -12,47 +12,45 @@
 
 * [DONE] What's the longest real word to show up in the totality of comics?
 
-* How many comics has no stings in them?
+* [DONE] How many comics has no transcripts to them?
 
-* Which comic contains the most strings?
-
-
-
-* What's the most common strings? Visualize the x most common words.
-  - Why are certain words more common? Exclude them and see your new result.
-
-* What's the average word count? Median?
-
-* Can I plot the word count across time?
-
-* How many words are in all caps? What are they?
-    Make sure to include phrases and which comic they are in (as a set).
-
-* Which comic has the most words in all caps?
+* [DONE] What's the most common string? Visualize the x most common words.
 
 
+-------DATES-------
+* [DONE] From what years/dates were the comics running from?
 
-* How many words begin with the letter (insert letter here)?
+* [DONE] How many comics were created in each year of publication?
+
+* [DONE] Plot the total amount of comics published each year.
+
+* [DONE] What's the average publication rate each year, excluding the current year?
+
+* [DONE] What's the median amount of comics published each year, excluding the current year?
+
+* What's the longest gap time(s) b/w comics?
+
+* What's the average gap times b/w comics?
+
+* What's the median gap times b/w comics?
+
+* What's the count of gap time(s)?
+
+* Plot the gap times in descending order.
+
 
 -------FIELDS-------
 * [DONE] How many comics use the links key?
 
-* Of these links, what are the unique domain names? Plot them
-
 * [DONE] How many comics use the news key?
 
-* Is there a comic number that skips the sequence? (answer: 404)
-
-* What countries have been mentioned in the comic? How many times?
-
-* Same for states/territories/cities.
-
-* Plot these on scattergeo.
-
+* [DONE] Comic 404 does not exist. Are there any other comics that don't exist in the sequence?
 
 """
 from xkcd_catalog_functions import *
 import time
+import matplotlib.pyplot as plt
+import datetime
 
 # Load our json file which contains all of our comics
 filename = 'all_xkcd.json'
@@ -63,53 +61,6 @@ with open(filename) as f:
 is_xkcd_updated(full_xkcd_repo)
 
 # The Data Analysis of our xkcd.json
-"""
------WORDS-----
-STEPS:
-1. Set a variable equal to the phrase. 
-2. Lowercase and split every word when storing it inside the variable.
-3. Clean the variable of punctuation marks
-4. Append all the words to a single list for later manipulation.
-5. Maybe store it in a json so I don't have to keep looping.
-
-This clean list can then be used for further analysis.
-
-Total word count:
-1. Get the word count via the len of the list
-
-Count of all words;
-1. In our list, count how many times a word shows up in our list and store it in a dictionary.
-2. Organize the dictionary by it's values, also make sure to deal with ties.
-3. Plot the results as a I see fit.
-2. Maybe even write our results to a .json so I don't have to loop it everytime. 
-
-Total unique words:
-1. Use len(set(my_list)) to get how many unique words are in the list
-
-Banned word list:
-1. Loop a del method to remove banned words or phrases from our list.
-  -Keep in mind that since every word has been split, I'd have to grab the og list, ban the phrases (ex. alt title),
-  then split the words. Else I'll remove the word title when it might actually have a use case
-
-Longest word:
-1. Store the first word in the list.
-2. Get the len of each following word in the list via len(my_list[index]). 
-3. If the len of the word is greater than my variable, that's my new longest word.
-4. Based on the results, make an .isalpha check so that words with numbers aren't part of the equation.
-5. Take into account comics that lead to an external site.
-
-Word with the most vowels:
-1. Get a word. 
-2. Count how many vowels it has. 
-3. Store the word and the count as in a dictionary as two different keys
-4. Make sure there's an isalpha check.
-5. Replace the word if there's a word with more vowels.
-6. Variant of this is to count the vowels of every word and then plot the words with the most vowels.
-7. Also note what the comic num was? That'd require a new approach with storing my list. 
-It'd probably have to be a dictionary that stores the comic num and the split words as two different keys.
-Then that'd be stored into a list full of dictionaries.
-"""
-
 """
 ------CREATING OUR MASTER WORD LIST----
 I need to create a list full of all the words used in each comic so that I can do data analysis of the words.
@@ -142,17 +93,10 @@ for list_item in words:
     split_word = list_item.lower().split()
     split_words.append(split_word)
 
-"""
-Test code to see if it works
-print(len(split_words))
-print(split_words[0])
-print(type(split_words[0]))
-"""
 
 # We store each word in each transcript into a single list, removing the 2D collection to just make one big list
 # We also strip the special characters so that we get a better sense of what words are repeated
 # (ex. 'alt', 'alt]]', and '{{alt' are different words
-
 split_words_in_list = []
 special_characters = '\'.[]"()<>?!@#$%^&*=+-_/{}\\|:;,`~'
 for list_item in split_words:
@@ -162,7 +106,6 @@ for list_item in split_words:
             split_words_in_list.append(y)
 
 """------WORDS ANALYSIS----"""
-
 print("\nHow many total split strings are there?")
 print(f"\tThere are {len(split_words_in_list)} total split strings.")
 
@@ -181,32 +124,168 @@ print(f"\tThe string is {long_word}, with a total length of {len(long_word)} cha
 
 print("\nWhat's the longest string with only characters that's actually a real word?")
 long_word = ''
-for word in set_split_words_in_list:
-    if len(word) > len(long_word):
-        time.sleep(0.5)  # Needed b/c otherwise, the API call can't keep up
-        if is_real_word(word):
-            long_word = word
+if input() == 'y':
+    for word in set_split_words_in_list:
+        if len(word) > len(long_word):
+            time.sleep(0.5)  # Needed b/c otherwise, the API call can't keep up
+            if is_real_word(word):
+                long_word = word
 
 print(f"\tThe word is {long_word}, with a total length of {len(long_word)} characters.")
 
+print("\nWhat are top 25 most used words?")
+word_count = {}
+for word in split_words_in_list:
+    if word in word_count:
+        word_count[word] += 1
+    else:
+        word_count[word] = 1
+
+sorted_word_count = dict(sorted(word_count.items(), key=lambda item: (item[1], item[0]), reverse=True))
+top_25_words = dict(list(sorted_word_count.items())[:26])
+print(f"The top 25 words are as follows:\n{top_25_words}")
+
+print(f"Let's plot the top 25 words!")
+pixels_width = 1920
+pixels_height = 1080
+dpi = 100
+fig_size = (pixels_width/dpi, pixels_height/dpi)
+
+x_values = list(top_25_words.keys())
+y_values = list(top_25_words.values())
+
+fig_01 = plt.figure()
+fig_01.set_size_inches(fig_size)
+
+plt.style.use('seaborn-v0_8')
+plt.bar(x_values, y_values)
+
+plt.title("Top 25 Repeated Words in XKCD Comics", fontsize=24, pad=20, fontweight='bold')
+plt.xlabel("Words", fontsize=18, fontweight='bold')
+plt.xticks(rotation=45, ha='right', fontsize=16)
+plt.ylabel("Amount", fontsize=18, labelpad=20, fontweight='bold')
+plt.yticks(fontsize=16)
+
+for i, value in enumerate(y_values):
+    plt.text(i, value, str(value), ha='center', va='bottom', fontsize=14)
+
+plt.savefig('Top 25 Words in XKCD Comics')
+
+print("\nHow many comics have no transcripts to them?")
+empty_comics = []
+for dict_item in full_xkcd_repo:
+    if not dict_item['transcript']:
+        empty_comics.append(dict_item)
+
+print(f"\tThere are {len(empty_comics)} comics with no transcripts to them.")
+
+
+"""-----DATS ANALYSIS-----"""
+print("\nHow long has the comic been running?")
+
+start_date = datetime.date(int(full_xkcd_repo[0]['year']),
+                           int(full_xkcd_repo[0]['month']),
+                           int(full_xkcd_repo[0]['day']))
+
+latest_date = datetime.date(int(full_xkcd_repo[-1]['year']),
+                            int(full_xkcd_repo[-1]['month']),
+                            int(full_xkcd_repo[-1]['day']))
+difference = latest_date - start_date
+days_in_year = 365.25
+years = difference.days / days_in_year
+print(f"The first comic was published on {start_date} and the latest comic was published on {latest_date}.")
+print(f"The comic has been running for {int(years)} years!")
+
+print("\nHow many comics were made in each year?")
+comic_years = {}
+for dict_item in full_xkcd_repo:
+    comic_year = dict_item['year']
+    if comic_year in comic_years:
+        comic_years[comic_year] += 1
+    else:
+        comic_years[comic_year] = 1
+print("The following is how many comics were made each year:")
+for year, amount in comic_years.items():
+    print(f"\tYear {year}: {amount} comics")
+
+print("\nTime to plot the amount of comics published each year!")
+year_values = list(comic_years.keys())
+total_comics_each_year = list(comic_years.values())
+
+fig_02 = plt.figure()
+fig_02.set_size_inches(fig_size)
+
+plt.bar(year_values, total_comics_each_year)
+
+plt.title("XKCD Comics Released By Year", fontsize=24, pad=20, fontweight='bold')
+plt.xlabel("Year", fontsize=18, labelpad=20, fontweight='bold')
+plt.xticks(fontsize=16)
+plt.ylabel("Amount", fontsize=18, labelpad=20, fontweight='bold')
+plt.yticks(fontsize=16)
+
+for index, value in enumerate(total_comics_each_year):
+    plt.text(index, value, str(value), ha='center', va='bottom', fontsize=16)
+
+plt.savefig('Total XKCD Comics Released By Year')
+
+print("\nWhat's the average amount of comics published each year, excluding the current year?")
+total = sum(total_comics_each_year[:-1])
+average = total/len(total_comics_each_year)-1
+
+print(f"\tThe average comics published each year is {average:.2f} comics.")
+
+print("\nWhat's the median number of comics published each year, excluding the current year")
+sorted_yearly_comic_rate = sorted(total_comics_each_year[:-1])
+n = len(sorted_yearly_comic_rate)
+median = 0
+if n % 2 == 1:
+    median = sorted_yearly_comic_rate[n//2]
+elif n % 2 == 0:
+    median = (sorted_yearly_comic_rate[(n//2)-1] + sorted_yearly_comic_rate[n//2]) / 2
+
+print(f"\tThe median number of comics published each year is {median} comics.")
+
+print("\nWhat's the longest gap time between comics?")
+comic_years_difference = {}
+# Get the date of the comic
+# Turn the date into a datetime object
+# Store the comic num and date into a dictionary
+
+# Get the difference of element n and element n-1
+# Store the difference, the n-1 date and the nth date into another dictionary
+
+# Once that is done, see which is the greatest difference. Make sure to account for ties
+# Based on the results, see if there's some plotting potential, like avg, median, mean of when comics get published
+for dict_item in full_xkcd_repo:
+    comic_year = dict_item['year']
+    if comic_year in comic_years:
+        comic_years[comic_year] += 1
+    else:
+        comic_years[comic_year] = 1
+print("The following is how many comics were made each year:")
+for year, amount in comic_years.items():
+    print(f"\tYear {year}: {amount} comics")
+
+
+
+
+
+
+
+
 
 """-----FIELDS ANALYSIS-----"""
+print("\nHow many comics use the news key?")
 news_count = 0
 xkcd_news = []
 for dict_item in full_xkcd_repo:
     if dict_item['news'] != '':
         xkcd_news.append(dict_item)
         news_count += 1
-        """
-        print(f"Comic num: {dict_item['num']}")
-        print(f"News: {dict_item['news']}")
-        print(f"Link: https://xkcd.com/{dict_item['num']}\n")"""
 
-print("\nHow many comics use the news key?")
 print(f"\tThere are {news_count} comics where the news key is used in the comic.")
 
-"""Are there any that repeats? What are they?"""
-
+print("\nHow many comics use the link key?")
 link_count = 0
 xkcd_links = []
 for dict_item in full_xkcd_repo:
@@ -214,9 +293,21 @@ for dict_item in full_xkcd_repo:
         xkcd_links.append(dict_item)
         link_count += 1
 
-print("\nHow many comics use the link key?")
 print(f"\tThere are {link_count} comics where the link key is used for the comic.")
 
 
+print(f"\nComic 404 does not exist. Are there any other comics that don't exist in the sequence?")
+comic_num = 0
+comic_sequence = []
+for dict_item in full_xkcd_repo:
+    comic_num += 1
+    if comic_num != dict_item['num']:
+        # Debug code:
+        # print(f"\nComic num {comic_num} does not match {dict_item['num']}")
+        # print(f"That comic doesn't exist!!!")
 
+        # Append the missing comic num to a list, correct comic num
+        comic_sequence.append(comic_num)
+        comic_num = dict_item['num']
 
+print(f"\tComic(s) {comic_sequence} skips the sequence!")
